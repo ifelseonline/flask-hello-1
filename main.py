4dc0cadd70fe0f062388d2d0f940673b8ab408e5
IFelse Online <63430704+ifelseonline@users.noreply.github.com>
2021-09-29 23:37:19 +0000

    Adicionada funcionalidade de reconhecimento de face

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 import os
-from flask import Flask
+import json
+import pickle
+import sklearn
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return """
-    <h1>Hello World!</h1>
-    <a href="/about">Sobre</a>
-    <br>
-    <a href="/contact">Contato</a>
-  """
+  return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return 'Sobre mim'
+@app.route('/upload', methods=['POST'])
+def upload():
+  try:
+    # Receber o arquivo do formulário
+    file = request.files['file']
 
-@app.route('/contact')
-def contact():
-  return 'Contato'
+    # Enviar o arquivo para a API
+    response = requests.post(
+      'https://face.ifelseonline.com.br/encoding',
+      files={'file': (
+        file.filename,
+        file.stream,
+        file.content_type,
+        file.headers
+      )}
+    )
+
+    # Decodificar resposta
+    data = json.loads(response.text)
+
+    # Verificar se a reposta teve sucesso
+    if not data['success']:
+      return render_template(
+        'predict.html',
+        predict='Sem faces'
+      )
+
+    # Enviar as caracteristicas para o modelo
+    face_encoding = data['encodings']
+    clf = pickle.load(open('clf.pickle', 'rb'))
+    predict = clf.predict([face_encoding])[0]
+  except Exception:
+    return render_template(
+      'predict.html',
+      predict='Erro ao reconhecer face'
+    )
+
+  return render_template(
+    'predict.html',
+    predict=predict
+  )
 
 if __name__ == '__main__':
   port = int(
